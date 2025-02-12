16bb0b0852e4263c36f36ff642dc9a20fde269fa
luzhihong <xiaopikaqiu@outlook.com>
2025-02-12 11:31:26 +0800

    useEffect

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 
 const LikeButton = () => {
   const [like, setLike] = useState(0);
   const [on,setOn] = useState(true);
+  useEffect(()=>{
+    document.title= `click ${like}`
+  })
   return (<><button onClick={()=>{setLike(like+1)}}>{like} LIKE</button><button onClick={()=>setOn(!on)}>{on?'on':'off'}</button></>);
 };
 export default LikeButton;
